a40d0024ac3a1d4db717e09bb3cce51769ef0d69
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2018-02-26 15:52:00 -0500

    Tweak install.sh and Brewfile

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 
-git pull origin master;
+# Prompt for admin password
+sudo -v;
 
-brew bundle;
+brew bundle --file=$HOME/.archive/Brewfile;
 brew cleanup;
 brew cask cleanup;
 
-# Copy files from current directory to `~`
-rsync --exclude ".git/" \
-      --exclude ".DS_Store" \
-      --exclude ".macos" \
-      --exclude "com.googlecode.iterm2.plist" \
-      --exclude "install.sh" \
-      --exclude "Monokai Soda.itermcolors" \
-      --exclude "README.md" \
-      -avh --no-perms . ~;
-
 # Link our iTerm preferences.
-# ln -s com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+rm -rf ~/Library/Preferences/com.googlecode.iterm2.plist
+ln -s com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
 source ~/.bash_profile;
