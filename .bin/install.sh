7308b6ee6a633c7fcbce478e64f5fd20e0bc34ba
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2018-03-01 15:34:40 -0500

    Add echoes to install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-source ~/.bin/echoes.sh
+ESC="\x1b["
+RESET=$ESC"39;49;00m"
+RED=$ESC"31m"
+GREEN=$ESC"32m"
+YELLOW=$ESC"33m"
+BLUE=$ESC"34m"
+MAGENTA=$ESC"35m"
+CYAN=$ESC"36m"
+
+function info() {
+  echo "\n$CYAN$1$RESET"
+}
+
+function ok() {
+  echo "$GREEN$1$RESET"
+}
+
+function warn() {
+  echo "$YELLOW$1$RESET"
+}
+
+function error() {
+  echo "$RED$1$RESET"
+}
 
 ########################################################################
 # Install Xcode Command Line Tools                                     #
