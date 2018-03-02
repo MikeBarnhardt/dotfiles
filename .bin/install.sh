f675326f5dbe3540026799ee238c067e10d530d7
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2018-03-02 12:10:46 -0500

    Remove macOS script; run Homebrew first

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,49 +1,42 @@
 #!/usr/bin/env bash
 
-ESC="\x1b["
-RESET=$ESC"39;49;00m"
-RED=$ESC"31m"
-GREEN=$ESC"32m"
-YELLOW=$ESC"33m"
-BLUE=$ESC"34m"
-MAGENTA=$ESC"35m"
-CYAN=$ESC"36m"
+########################################################################
+# Echoers                                                              #
+########################################################################
 
+# Display an info message
 function info() {
-  echo "\n$CYAN$1$RESET"
+  echo "\033[44m\033[30m INFO \033[39;49;00m \033[34m$1\033[39;49;00m"
 }
 
+# Display a success message
 function ok() {
-  echo "$GREEN$1$RESET"
+  echo "\033[42m\033[30m OKAY \033[39;49;00m \033[32m$1\033[39;49;00m"
 }
 
+# Display a warning message
 function warn() {
-  echo "$YELLOW$1$RESET"
+  echo "\033[43m\033[30m WARN \033[39;49;00m \033[33m$1\033[39;49;00m"
 }
 
+# Display an error message
 function error() {
-  echo "$RED$1$RESET"
+  echo "\033[45m\033[30m FAIL \033[39;49;00m \033[35m$1\033[39;49;00m"
 }
 
 ########################################################################
-# Install Xcode Command Line Tools                                     #
+info "Installing Homebrew"
 ########################################################################
 
-info "Installing Xcode Command Line Tools"
-if ! xcode-select -p &> /dev/null; then
-  xcode-select --install &> /dev/null
-  wait
-
-  # Prompt to accept Xcode license agreement.
-  info "Agree to the terms of the Xcode license?"
-  sudo xcodebuild -license accept &> /dev/null
-  wait
+# Homebrew automatically installs Xcode Command Line Tools
+if ! which brew > /dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  ok "Xcode Command Line Tools already installed!"
-fi
+  ok 'Homebrew is already installed.'
+fi;
 
 ########################################################################
-# Setup dotfiles repository                                            #
+info "Setting up dotfiles"
 ########################################################################
 
 # A shortcut replacement for `git` that will use our home directory as
@@ -54,20 +47,17 @@ function dot {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-info "Setting up dotfiles"
 if which git &> /dev/null; then
   if [[ ! -d $HOME/.dotfiles ]]; then
     # Set up a `--bare` repository. This results in a directory that
     # functions like a `.git` folder. This way we can keep our files
     # directly in `$HOME`.
-    info "Fetching the dotfiles repository."
+    info 'Cloning repository `.dotfiles`'
     git clone --bare https://github.com/MikeBarnhardt/dotfiles.git $HOME/.dotfiles
-    wait
 
     # Checkout and replace any conflicting files.
-    info "Copying dotfiles from repository to home folder"
+    info 'Checking out `.dotfiles`'
     dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs rm -rf
-    wait
 
     # Don't show untracked files. Since `$HOME` is our working directory
     # we only want to manually add files to track.
@@ -75,44 +65,27 @@ if which git &> /dev/null; then
     # Do not use `dot add .` or `dot commit -a` under any circumstances.
     dot config status.showUntrackedFiles no
   else
-    error "$HOME/.dotfiles already exists. Refusing to clone repository."
+    warn 'Directory `.dotfiles` already exists'
   fi
 else
   error "Can't fetch repository: `git` is not installed."
 fi
 
 ########################################################################
-# Install Homebrew                                                     #
+info "Installing Homebrew Applications"
 ########################################################################
-
-# Test if Homebrew is already installed; if not, install it.
-info "Installing Homebrew"
-# Install Homebrew if not already installed
-if ! which brew > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  wait
-else
-  ok 'Homebrew is already installed.'
-fi;
 
 # Using a global Brewfile (located in the users's home directory) we
 # automatically install all our programs.
-info "Installing from Brewfile"
 if [ -f $HOME/.Brewfile ]; then
   brew bundle --global
-  wait
 else
   error "$HOME/.Brewfile does not exist."
 fi
 
 # Remove installation files afterwards.
-info "Cleaning up Homebrew files."
 brew cleanup
 brew cask cleanup
-wait
 
 # Copy our iTerm preferences.
-# cp $HOME/.extra/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist 2> /dev/null
-
-# Setup macOS defaults
-# sh $HOME/.macos
+cp $HOME/.extra/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist 2> /dev/null
